163fd821d828bb16150a9c17a853b098a767c2f6
grqphical <95062977+grqphical@users.noreply.github.com>
2024-12-01 21:32:19 -0800

    finished part two of day 2

--- day-2.py
+++ day-2.py
@@ -40,5 +40,46 @@ for report in reports:
 
     if is_safe:
         safe_report_count += 1
+print(safe_report_count)
+
+# PART TWO
+
+
+def is_safe_sequence(numbers):
+    direction = 0
+    for i in range(len(numbers) - 1):
+        if abs(numbers[i] - numbers[i + 1]) not in [1, 2, 3]:
+            return False
+        if abs(numbers[i] - numbers[i + 1]) == 0:
+            return False
+        if direction == 0:
+            if numbers[i + 1] > numbers[i]:
+                direction = 1
+            elif numbers[i + 1] < numbers[i]:
+                direction = -1
+        else:
+            if numbers[i + 1] > numbers[i] and direction == -1:
+                return False
+            elif numbers[i + 1] < numbers[i] and direction == 1:
+                return False
+    return True
+
+
+safe_report_count = 0
+
+for report in reports:
+    numbers = [int(i) for i in report.split()]
+    is_safe = is_safe_sequence(numbers)
+
+    if not is_safe:
+        # Try removing each number one at a time
+        for i in range(len(numbers)):
+            test_numbers = numbers[:i] + numbers[i + 1 :]
+            if is_safe_sequence(test_numbers):
+                is_safe = True
+                break
+
+    if is_safe:
+        safe_report_count += 1
 
 print(safe_report_count)
