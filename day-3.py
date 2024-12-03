cdb517832570c51cb554867095756a4558b01b5b
grqphical07 <95062977+grqphical07@users.noreply.github.com>
2024-12-02 21:56:38 -0800

    finished part two

--- day-3.py
+++ day-3.py
@@ -13,3 +13,32 @@ for match in matches:
     sum += int(match[0]) * int(match[1])
 
 print(sum)
+
+# PART TWO
+with open("day-3-input.txt", "r") as f:
+    data = f.read()
+
+mul = re.compile(r"mul\((\d*),(\d*)\)")
+matches = mul.findall(data)
+mul_positions = [(match.start(), "mul") for match in mul.finditer(data)]
+do_positions = [(match.start(), "do") for match in re.finditer(r"do\(\)", data)]
+dont_positions = [(match.start(), "dont") for match in re.finditer(r"don't\(\)", data)]
+
+instructions = sorted(mul_positions + do_positions + dont_positions, key=lambda x: x[0])
+instructions = [instr[1] for instr in instructions]
+
+mul_counter = 0
+sum = 0
+enabled = True
+for instruction in instructions:
+    if instruction == "mul":
+        if enabled:
+            sum += int(matches[mul_counter][0]) * int(matches[mul_counter][1])
+        mul_counter += 1
+
+    elif instruction == "dont":
+        enabled = False
+    elif instruction == "do":
+        enabled = True
+
+print(sum)
